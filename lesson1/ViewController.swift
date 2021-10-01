1ef7c6d3329c167454d1e40880a4758f6a0f8922
Rufina Gataullina <mufwoo@gmail.com>
2021-10-01 15:19:49 +0300

    Initial Commit

---
+++ lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Rufus on 01.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
