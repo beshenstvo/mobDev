8a106fb4b3f76ea9827ead8c3541d73e3e5965c9
Rufina Gataullina <mufwoo@gmail.com>
2021-10-07 19:21:32 +0300

    saving

--- lesson1/ViewController.swift
+++ lesson1/ViewController.swift
@@ -14,6 +14,21 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    var count: Int? = 0;
+    @IBAction func click(_ sender: UIButton) {
+        let randomInt: String? = String(Int.random(in: 1..<7))
+        let myImage: UIImage = UIImage(named: randomInt!)!
+        image.image = myImage
+        myLabel.text = "Cчёт: \(count!+Int(randomInt!)!)"
+        count! += Int(randomInt!)!
+    }
+    
+    @IBAction func clear(_ sender: UIButton) {
+        count = 0;
+        myLabel.text = "Cчёт: 0"
+        image.image =  UIImage(named: "0")
+    }
 }
 
